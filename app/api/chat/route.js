da772aa0345ed51a8a5c00b047d25ef3178c71ee
ShaarifAliSyed <shaarifalisyed@outlook.com>
2024-08-11 22:06:50 -0400

    Fixed bugs, app works as intended

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 // Can be changed for now this is a support bot for large retail company specializing in electronics, home appliances, and accessories
 const systemPrompt = `You are a customer support assistant for a large retail company specializing in electronics, home appliances, and accessories. Your goal is to assist customers by answering their questions, resolving their issues, and providing helpful information.
@@ -16,36 +17,25 @@ const systemPrompt = `You are a customer support assistant for a large retail co
 Your goal is to ensure every customer feels heard, valued, and satisfied with the service provided.`
 
 export async function POST(req) {
-    const openai = new OpenAI()
-    const data = await req.json()
-    const completion = await openai.chat.completions.create({
-        messages: [
-            {
-                role: 'system', content: systemPrompt
-            },
-            ...data, // Spread operator to get the rest of the messages 
-        ],
-        model: "gpt-4o-mini",
-        stream: true,
-    })
-    const stream = new ReadableStream({
-        async start(controller) {
-            const encoder = new TextEncoder()
-            try {
-                for await (const chunk of completion) { // OpenAI sends you info in chunks so wait for all chunks to arrive
-                    const content = chunk.choices[0]?.delta?.content
-                    if (content) {
-                        const text = encoder.encode(content)
-                        controller.enqueue(text) 
-                    }
-                }
-            }
-            catch(err) {
-                controller.error(err)
-            } finally {
-                controller.close()
-            }
+    const data = await req.json();
+
+    // Initialize the GoogleGenerativeAI client with your API key
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+    const result = await model.generateContent(systemPrompt + "\n" + data.map(message => `${message.role}: ${message.content}`).join("\n"))
+    const response = await result.response
+    const text = await response.text()
+
+    // Clean up astericks 
+    const finalText = text 
+    .replace("assistant: ", "")
+    .replace(/\n$/, "")
+    .replace(/\*/g, "")
+
+    return new NextResponse(finalText, {
+        headers: {
+            'Content-Type': 'text-plain'
         }
     })
-    return new NextResponse(stream)
 }
